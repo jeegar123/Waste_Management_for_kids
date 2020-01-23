473cf16e046fcef0b887fecb7078cff19b544ce1
HARSH <hvekariya889@rku.ac.in>
2020-01-23 22:25:13 +0530

    update

--- app/src/main/java/com/app/wastemanagementforkids/study/Study2Activity.java
+++ app/src/main/java/com/app/wastemanagementforkids/study/Study2Activity.java
@@ -3,7 +3,6 @@ package com.app.wastemanagementforkids.study;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,23 +10,19 @@ import com.app.wastemanagementforkids.R;
 
 public class Study2Activity extends AppCompatActivity {
 
-    Spinner spinner;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study2);
-        spinner = findViewById(R.id.lang_spinner);
     }
 
-    public void langUpdate(View view) {
-        String langauage = spinner.getSelectedItem().toString();
-        if (langauage.equals("English")) {
-            startActivity(new Intent(Study2Activity.this, StudyActivity.class).putExtra("lang", "English"));
-            finish();
-        } else if (langauage.equals("Gujarati")) {
-            startActivity(new Intent(Study2Activity.this, StudyActivity.class).putExtra("lang", "Gujarati"));
-            finish();
-        }
+    public void english(View view) {
+        startActivity(new Intent(Study2Activity.this, StudyActivity.class).putExtra("lang", "English"));
+        finish();
+    }
+
+    public void gujarati(View view) {
+        startActivity(new Intent(Study2Activity.this, StudyActivity.class).putExtra("lang", "Gujarati"));
+        finish();
     }
 }
