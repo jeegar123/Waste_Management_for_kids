94acb0da117fccec9c81c7f2e3020cd5b01c398e
jeegarlearner <50761184+jeegarlearner@users.noreply.github.com>
2020-01-24 10:36:57 +0530

    update

--- app/src/main/java/com/app/wastemanagementforkids/SplashActivity.java
+++ app/src/main/java/com/app/wastemanagementforkids/SplashActivity.java
@@ -23,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
                 finish();
             }
-        }, 3000);
+        }, 1000);
 
     }
 }
