9605c401ff7a53335b6d609c225ab7e8e7d4f297
jeegarlearner <50761184+jeegarlearner@users.noreply.github.com>
2020-01-24 08:41:13 +0530

    update

--- app/src/main/java/com/app/wastemanagementforkids/game/GameHomeActivity.java
+++ app/src/main/java/com/app/wastemanagementforkids/game/GameHomeActivity.java
@@ -1,6 +1,8 @@
 package com.app.wastemanagementforkids.game;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,24 @@ public class GameHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_home);
+    }
+
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.text_home_quiz:
+                startActivity(new Intent(GameHomeActivity.this, GameActivity.class));
+                break;
+            case R.id.text_home_quiz2:
+                startActivity(new Intent(GameHomeActivity.this, QuizGameActivity.class));
+                break;
+            case R.id.text_home_webgame1:
+                startActivity(new Intent(GameHomeActivity.this, WebGame1Activity.class));
+                break;
+            case R.id.text_home_webgame2:
+                startActivity(new Intent(GameHomeActivity.this, WebGame2Activity.class));
+                break;
+        }
+
     }
 }
