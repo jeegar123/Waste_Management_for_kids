4f739bce832ecc4ca61e2588c028b85c4dbd4dc4
jeegarlearner <50761184+jeegarlearner@users.noreply.github.com>
2020-01-24 08:26:20 +0530

    update

---
+++ app/src/main/java/com/app/wastemanagementforkids/game/GameHomeActivity.java
@@ -0,0 +1,16 @@
+package com.app.wastemanagementforkids.game;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.app.wastemanagementforkids.R;
+
+public class GameHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_home);
+    }
+}
