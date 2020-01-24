b54406a236474d10a9a54d6304cda47cb1f472fc
HARSH <hvekariya889@rku.ac.in>
2020-01-24 11:59:18 +0530

    updated

--- app/src/main/java/com/app/wastemanagementforkids/study/Study2Activity.java
+++ app/src/main/java/com/app/wastemanagementforkids/study/Study2Activity.java
@@ -5,15 +5,19 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.app.wastemanagementforkids.R;
 
 public class Study2Activity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study2);
+        toolbar = findViewById(R.id.study2_toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void english(View view) {
